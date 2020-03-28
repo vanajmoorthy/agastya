2234392a01fe82683659a586b2b1e55f347087ce
vanajmoorthy <vanajmoorthy@gmail.com>
2020-03-28 07:52:21 +0530

    aded from

---
+++ src/components/Form.vue
@@ -0,0 +1,24 @@
+<template>
+	<form name="contact" method="POST" data-netlify="true">
+        <h3>Say something nice about aggsu</h3>
+		<p>
+			<label>Your Name: <input type="text" name="name"/></label>
+		</p>
+		<p>
+		</p>
+		<p>
+		</p>
+		<p>
+			<label>Message: <textarea name="message"></textarea></label>
+		</p>
+		<p>
+			<button type="submit">Send</button>
+		</p>
+	</form>
+</template>
+
+<script>
+export default {
+  name: "Form"
+};
+</script>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="cont">
     <Contact />
+    <Form />
   </div>
 </template>
 
 <script>
 import Contact from "@/components/Contact.vue";
+import Form from "@/components/Form.vue";
+
 
 export default {
   name: "cont",
   components: {
     Contact
+  },
+  name: "for",
+  components: {
+    Form
   }
 };
+
 </script>
